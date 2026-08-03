4265bab528a622f2671ab748ea9b1d0175882a4d
SANTHOSH R <santhoshsrs2007@gmail.com>
2026-08-03 23:56:00 +0530

    Time: 27 ms (40.61%) | Memory: 111.1 MB (19.77%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> seen = new HashMap<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (seen.containsKey(nums[i])) {
+                if (i - seen.get(nums[i]) <= k) {
+                    return true;
+                }
+            }
+            seen.put(nums[i], i);
+        }
+        
+        return false;
+    }
+}
